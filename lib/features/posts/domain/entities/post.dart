f6bb75b1595a7e2faee2391fcbe3b519e8fcd1ac
Sandaru Perera <anuththaraperera2003@gmail.com>
2026-05-08 12:46:01 +0530

    feat(domain): add Post entity, repository interface, and GetPosts usecase

--- lib/features/posts/domain/entities/post.dart
+++ lib/features/posts/domain/entities/post.dart
@@ -5,13 +5,4 @@ class Post {
   final String body;
 
   Post({required this.id, required this.userId, required this.title, required this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      id: json['id'] as int,
-      userId: json['userId'] as int,
-      title: json['title'] as String,
-      body: json['body'] as String,
-    );
-  }
 }
