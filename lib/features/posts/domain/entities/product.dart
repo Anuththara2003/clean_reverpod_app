cb1368bcf3ec0f8ce3208b67902d97aba6770911
Sandaru Perera <anuththaraperera2003@gmail.com>
2026-05-08 11:06:40 +0530

    feat: add Product and CartItem entities

---
+++ lib/features/posts/domain/entities/product.dart
@@ -0,0 +1,14 @@
+class Product {
+  final String id;
+  final String name;
+  final double price;
+
+  Product({required this.id, required this.name, required this.price});
+}
+
+class CartItem {
+  final Product product;
+  final int quantity;
+
+  CartItem({required this.product, required this.quantity});
+}
