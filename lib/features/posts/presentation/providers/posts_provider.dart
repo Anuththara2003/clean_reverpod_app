857296741bf43f5ccd3979a5951b8235a9817352
Sandaru Perera <anuththaraperera2003@gmail.com>
2026-05-08 11:30:08 +0530

    feat: add missing post entity and fix provider import

--- lib/features/posts/presentation/providers/posts_provider.dart
+++ lib/features/posts/presentation/providers/posts_provider.dart
@@ -2,10 +2,17 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../domain/entities/post.dart';
 
-final dioProvider = Provider((ref) => Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com')));
+
+final dioProvider = Provider((ref) => Dio(
+  BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com'),
+));
+
 
 final postsProvider = FutureProvider<List<Post>>((ref) async {
   final dio = ref.watch(dioProvider);
   final response = await dio.get('/posts');
-  return (response.data as List).map((e) => Post.fromJson(Map<String, dynamic>.from(e))).toList();
+
+
+  final List data = response.data;
+  return data.map((json) => Post.fromJson(json)).toList();
 });
