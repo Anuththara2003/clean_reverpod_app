c18d23b68c70dd0ba610103843d4aef2cce1f835
Sandaru Perera <anuththaraperera2003@gmail.com>
2026-05-08 11:32:27 +0530

    chore: update main.dart to show PostsListPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+// Import Pages
+import 'features/posts/presentation/pages/posts_list_page.dart';
 import 'features/posts/presentation/pages/shopping_page.dart';
+
+// Import Providers
 import 'features/posts/presentation/providers/theme_provider.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(
+
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
 }
 
 class MyApp extends ConsumerWidget {
@@ -12,16 +22,28 @@ class MyApp extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+
     final themeMode = ref.watch(themeModeProvider);
 
     return MaterialApp(
       title: 'Clean Riverpod App',
       debugShowCheckedModeBanner: false,
+
+      // Theme settings
       themeMode: themeMode,
-      theme: ThemeData(useMaterial3: true, brightness: Brightness.light),
-      darkTheme: ThemeData(useMaterial3: true, brightness: Brightness.dark),
-      // මෙතන CounterPage වෙනුවට ShoppingPage දාන්න
-      home: const ShoppingPage(),
+      theme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.light,
+        colorSchemeSeed: Colors.blue,
+      ),
+      darkTheme: ThemeData(
+        useMaterial3: true,
+        brightness: Brightness.dark,
+        colorSchemeSeed: Colors.blue,
+      ),
+
+
+      home: const PostsListPage(),
     );
   }
 }
