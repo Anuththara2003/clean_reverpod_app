0bf62fe9256ae26aa3c90be4c3dad76e67477719
Sandaru Perera <anuththaraperera2003@gmail.com>
2026-05-08 12:51:11 +0530

    feat(di): wire up clean architecture layers using Riverpod

--- lib/features/posts/presentation/providers/posts_provider.dart
+++ lib/features/posts/presentation/providers/posts_provider.dart
@@ -1,53 +1,32 @@
-import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../domain/entities/post.dart';
+import '../../data/repositories/post_repository_impl.dart';
+import '../../data/sources/post_remote_data_source.dart';
+import '../../domain/usecases/get_posts_usecase.dart';
 
-final dioProvider = Provider((ref) => Dio(BaseOptions(baseUrl: 'https://jsonplaceholder.typicode.com')));
+// 1. Dio Provider
+final dioProvider = Provider((ref) => Dio());
 
-// Posts List
-final postsProvider = FutureProvider.autoDispose<List<Post>>((ref) async {
+// 2. Data Source Provider
+final postRemoteDataSourceProvider = Provider((ref) {
   final dio = ref.watch(dioProvider);
-  final response = await dio.get('/posts');
-  final List data = response.data;
-  return data.map((e) => Post.fromJson(e)).toList();
+  return PostRemoteDataSourceImpl(dio);
 });
 
-// Search Notifier
-class SearchNotifier extends StateNotifier<AsyncValue<List<Post>>> {
-  final Ref ref;
-  SearchNotifier(this.ref) : super(const AsyncValue.data([]));
-
-  Future<void> search(String query) async {
-    if (query.isEmpty) {
-      state = const AsyncValue.data([]);
-      return;
-    }
-    state = const AsyncValue.loading();
-    try {
-      final dio = ref.read(dioProvider);
-      final response = await dio.get('/posts?title_like=$query');
-      final List data = response.data;
-      state = AsyncValue.data(data.map((e) => Post.fromJson(e)).toList());
-    } catch (e, st) {
-      state = AsyncValue.error(e, st);
-    }
-  }
-}
-
-final searchProvider = StateNotifierProvider.autoDispose<SearchNotifier, AsyncValue<List<Post>>>((ref) {
-  return SearchNotifier(ref);
+// 3. Repository Provider
+final postRepositoryProvider = Provider((ref) {
+  final remoteDataSource = ref.watch(postRemoteDataSourceProvider);
+  return PostRepositoryImpl(remoteDataSource);
 });
 
-// Detail & Comments
-final postDetailProvider = FutureProvider.autoDispose.family<Post, int>((ref, id) async {
-  final dio = ref.watch(dioProvider);
-  final response = await dio.get('/posts/$id');
-  return Post.fromJson(response.data);
+// 4. Use Case Provider
+final getPostsUseCaseProvider = Provider((ref) {
+  final repository = ref.watch(postRepositoryProvider);
+  return GetPostsUseCase(repository);
 });
 
-final commentsProvider = FutureProvider.autoDispose.family<List<dynamic>, int>((ref, postId) async {
-  final dio = ref.watch(dioProvider);
-  final response = await dio.get('/posts/$postId/comments');
-  return response.data as List;
+// 5. Final UI Provider
+final postsProvider = FutureProvider((ref) async {
+  final getPostsUseCase = ref.watch(getPostsUseCaseProvider);
+  return await getPostsUseCase();
 });
